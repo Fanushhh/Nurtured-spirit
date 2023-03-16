55ed691d18899f16f3af9ff9a31fe6c812b94880
Fanush <fgabriel.mihailescu@gmail.com>
2023-03-16 14:16:20 +0200

    contact feature added

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,27 +1,55 @@
+
 import './Form.scss';
+import React, { useRef } from 'react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
+import emailjs from '@emailjs/browser';
 const ReactPhoneInput = PhoneInput.default ? PhoneInput.default : PhoneInput;
 
 
 export default function Form(){
+    const form = useRef();
+
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_a4mjyf9', 'template_glu23fh', form.current, 'ZUFI60sRcmDZk0ocU')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      
+      alert("Email successfully sent");
+      e.target.reset();
+    }
+
+
+
+
     return(
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
             <div>
                 <label>Full Name
-                <input placeholder='Your full name' type='text' name='name' />
+                <input placeholder='Your full name' type='text' name='user_name' />
                 </label>
             </div>
             <div>
                 <label>
                     Email Address
-                    <input placeholder='Your email address' type='email' name='email' />
+                    <input placeholder='Your email address' type='email' name='user_email' />
                 </label>
             </div>
             <div>
                 <label className='phoneInput'>
                     Phone Number
-                    <ReactPhoneInput type='phone' name='phone' />
+                    <ReactPhoneInput  
+                    inputProps={{
+                        name: 'phone',
+                        required: true,
+                        autoFocus: false
+                    }}/>
                 </label>
             </div>
             <div>
@@ -30,7 +58,7 @@ export default function Form(){
                     <textarea rows={5} placeholder="Enter your message here..." type='text' name='message' />
                 </label>
             </div>
-            <button>Send</button>
+            <button type='submit' value='Send'>Send</button>
                 
             
         </form>
