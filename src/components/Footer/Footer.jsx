b2b27232664aa40549371ec5bdaea87d11e2e7dc
Fanush <fgabriel.mihailescu@gmail.com>
2023-03-20 10:57:29 +0200

    added new bg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Footer.scss';
 
 
@@ -14,9 +15,8 @@ export default function Footer(){
                     <p>Canberra, ACT</p>
                 </div>
                 <div className="socialsContainer">
-                    <img src='./images/fb-icon.svg' alt='socials icon' />
-                    <img src='./images/insta-icon.svg' alt='socials icon' />
-                    <img src='./images/linkedin-icon.svg' alt='socials icon' />
+                    <Link to='https://www.facebook.com/nurturedspirit'><img src='./images/fb-icon.svg' alt='socials icon' /></Link>
+                    <Link to='https://www.instagram.com/nurtured_spirit/?igshid=YmMyMTA2M2Y%3D'><img src='./images/insta-icon.svg' alt='socials icon' /></Link>
                 </div>
             </div>
             <div className='copyright'>
